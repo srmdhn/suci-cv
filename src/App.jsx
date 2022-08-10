d2d84d1c82ec98e06470accf22ea285c106938a9
Suci ramadhan <suci21005@gmail.com>
2022-08-10 16:20:28 +0700

    babs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/header/header'
 import Nav from './components/nav/nav'
 import About from './components/about/about'
@@ -10,12 +10,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 const App = () => {  
+  const [annyeong, setAnnyeong] = useState('안녕하세요 여러분')
+  const [rams, setRams] = useState('')
+  const [sram, setSrams] = useState('')
+  const [eon, setEon] = useState('')
+
+  const feel = {
+    annyeong, setAnnyeong, rams, setRams, sram, setSrams, eon, setEon
+  }
   return (
     <Router>
     <>
         <Nav />
         <Routes>
-        <Route exact path="/" element={<Header/>} />
+        <Route exact path="/" element={<Header feel={feel} />} />
         <Route exact path='/about' element={<About/>} />
         <Route exact path="/experience" element={<Experience/>} />
         <Route exact path="/portfolio" element={<Portfolio/>} />
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,13 +4,38 @@ import CTA from './CTA'
 import ME from '../../assets/tampil-removebg-preview.png'
 import HeaderSocials from './HeaderSocials'
 
-const header =() => {
+const header =({feel}) => {
+  const {annyeong, setAnnyeong} = feel
+  const {rams, setRams} = feel
+  const {sram, setSrams} = feel
+  const {eon, setEon} = feel
+
+function sapa(){
+    setAnnyeong ( "안녕하세요 여러분")
+}
+
+function identitas(){
+    setRams ("SMKN 1 KOTA BEKASI")
+}
+
+function sy(){
+  setSrams ("its Suci")
+}
+
+function ay(){
+  setEon ("GO")
+}
+
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello Saya</h5>
-        <h1>Suci Ramadhani</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <div className='title'>
+          <h5>{annyeong}</h5>
+          <h1>{rams}</h1>
+          <h5>{sram}</h5>
+          <h1>{eon}</h1>
+        </div>
+       <button className='btn btn-pimary' onClick={() => {sapa(); identitas(); sy(); ay(); }}><i><b>TAP</b></i></button>
         <CTA />
         <HeaderSocials />
 
